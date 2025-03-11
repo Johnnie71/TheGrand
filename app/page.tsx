d70d363bb96118d44f462544c8c13a39116edfae
Johnnie <67025540+Johnnie71@users.noreply.github.com>
2025-03-11 14:42:59 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="txt-2xl font-bold">Next TS Starter</h1>
+      </div>
+    </section>
+  
+  )
+}
+
+export default Home
